9258e232a217541825ff611dc05da069130054f6
Arjun Ramesh <arjunramesh@utexas.edu>
2021-01-17 18:00:25 +0530

    Switched out for behavioral sram RAT, initial impl 2 stage forwarding

--- behavioral/rat.v
+++ behavioral/rat.v
@@ -24,32 +24,43 @@ module rat(
   // rob interface
   input         rob_flush,
   input         rob_ret_valid,
-  input [5:0]   rob_ret_rd,
+  input [4:0]   rob_ret_rd,
   input [31:0]  rob_ret_result);
 
-  wire[31:0] comm_val_rs1;
-  wire[31:0] comm_val_rs2;
-  wire[6:0] tag_rs1;
-  wire[6:0] tag_rs2;
-  wire[31:0] spec_val_rs1;
-  wire[31:0] spec_val_rs2;
-  reg forward_rs1;
-  reg forward_rs2;
+  reg[31:0] comm_val_rs1;
+  reg[31:0] comm_val_rs2;
+  reg[6:0] tag_rs1;
+  reg[6:0] tag_rs2;
+  reg[6:0] tag_wb;
+  reg[31:0] spec_val_rs1;
+  reg[31:0] spec_val_rs2;
+  reg forward_cur_rs1;
+  reg forward_cur_rs2;
+  reg forward_prev_rs1;
+  reg forward_prev_rs2;
+
+  reg wb_write;
+  // Old CDB value
+  reg wb_prev_write;
+  reg[31:0] wb_prev_result;
+  reg[6:0] wb_prev_robid;
+  reg[4:0] wb_prev_rd; 
 
   reg valid_rs1;
   reg valid_rs2;
   reg committed_rs1;
   reg committed_rs2;
 
-  reg ld_comm_val;
   reg ld_tag;
   reg ld_spec_val;
-  reg ld_valid;
-  reg ld_committed;
  
   reg[31:0] rat_valid;
   reg[31:0] rat_committed;
-  
+ 
+  reg[31:0] rat_comm_val[31:0];
+  reg[6:0] rat_tag[31:0];
+  reg[31:0] rat_spec_val[31:0];
+  /*
   sram_rat #(.DATAW(32)) rat_comm_val (
     .clk(clk),
     .rst(rst),
@@ -59,8 +70,8 @@ module rat(
     .rd_en2(rename_rat_valid),
     .rd_addr2(rename_rat_rs2),
     .rd_data2(comm_val_rs2),
-    .wr_en(ld_comm_val),
-    .wr_addr(rob_ret_rd[4:0]),
+    .wr_en(rob_ret_valid),
+    .wr_addr(rob_ret_rd),
     .wr_data(rob_ret_result));
   
   sram_rat #(.DATAW(7)) rat_tag (
@@ -88,40 +99,87 @@ module rat(
     .wr_en(ld_spec_val),
     .wr_addr(wb_rd[4:0]),
     .wr_data(wb_result));
- 
+ */
+
+  always @(posedge clk) begin
+    // SRAM Behavioral
+    if (rename_rat_valid) begin
+      comm_val_rs1 <= rat_comm_val[rename_rat_rs1];
+      comm_val_rs2 <= rat_comm_val[rename_rat_rs2];
+      tag_rs1 <= rat_tag[rename_rat_rs1];
+      tag_rs2 <= rat_tag[rename_rat_rs2];
+      tag_wb <= rat_tag[wb_rd[4:0]];
+      spec_val_rs1 <= (wb_prev_write & (rename_rat_rs1 == wb_prev_rd)) ? 
+                        wb_prev_result : rat_spec_val[rename_rat_rs1];
+      spec_val_rs2 <= (wb_prev_write & (rename_rat_rs2 == wb_prev_rd)) ?
+                        wb_prev_result : rat_spec_val[rename_rat_rs2];
+    end
+    if (rob_ret_valid)
+      rat_comm_val[rob_ret_rd] <= rob_ret_result;
+    if (ld_tag)
+      rat_tag[rename_rat_rd[4:0]] <= rename_rat_robid[6:0];
+    if (ld_spec_val)
+      rat_spec_val[wb_rd[4:0]] <= wb_prev_result;
+  end
 
   always @(posedge clk) begin
     if (rst) begin
       rat_valid <= 32'hFFFFFFFF;
       rat_committed <= 32'hFFFFFFFF;
     end 
+    if (rename_rat_valid) begin
+      // Read control bits: Forward if insn in decode and 2nd wb cycle
+      valid_rs1 <= (wb_prev_write & (rename_rat_rs1 == wb_prev_rd)) ?
+                    1'b1 : rat_valid[rename_rat_rs1];
+      valid_rs2 <= (wb_prev_write & (rename_rat_rs1 == wb_prev_rd)) ? 
+                    1'b1 : rat_valid[rename_rat_rs2];
+      committed_rs1 <= rat_committed[rename_rat_rs1];
+      committed_rs2 <= rat_committed[rename_rat_rs2];
+    end
     // Write control bits
-    if (wb_valid) 
-      rat_valid[wb_rd[4:0]] <= 1;
-    if (rob_ret_valid)
-      rat_committed[rob_ret_rd[4:0]] <= 1;
+    if (ld_spec_val)
+      rat_valid[wb_prev_rd[4:0]] <= 1;
     if (rename_rat_valid & (~rename_rat_rd[5])) begin
       rat_valid[rename_rat_rd[4:0]] <= 0;
       rat_committed[rename_rat_rd[4:0]] <= 0;
     end  
-    // Read control bits
-    valid_rs1 <= rat_valid[rename_rat_rs1];
-    valid_rs2 <= rat_valid[rename_rat_rs2];
-    committed_rs1 <= rat_committed[rename_rat_rs1];
-    committed_rs2 <= rat_committed[rename_rat_rs2];
+  end
+
+  always @(posedge clk) begin
+    // Save CDB values
+    if (rst)
+      wb_prev_write <= 1'b0;
+    wb_prev_write <= wb_write;
+    wb_prev_robid <= wb_robid[6:0];
+    wb_prev_result <= wb_result;
+    wb_prev_rd <= wb_rd[4:0];
   end
 
   always @(*) begin
     ld_tag = rename_rat_valid & (~rename_rat_rd[5]);
-    ld_spec_val = wb_valid & (~wb_error) & (~wb_rd[5]);
-    ld_comm_val = rob_ret_valid & (~rob_ret_rd[5]);
-    // Forward value
-    forward_rs1 = ld_spec_val & (wb_result[4:0] == rename_rat_rs1);
-    forward_rs2 = ld_spec_val & (wb_result[4:0] == rename_rat_rs2);
-    rat_rs1_valid = forward_rs1 | valid_rs1;
-    rat_rs2_valid = forward_rs2 | valid_rs2;
-    rat_rs1_tagval = rat_rs1_valid ? (forward_rs1 ? wb_result : spec_val_rs1) : tag_rs1;
-    rat_rs2_tagval = rat_rs2_valid ? (forward_rs2 ? wb_result : spec_val_rs2) : tag_rs2;
+    wb_write = wb_valid & (~wb_error) & (~wb_rd[5]);
+    ld_spec_val = wb_prev_write & (wb_prev_robid == tag_wb);
+    // Forward value: Prev and Cur CDB val
+    forward_prev_rs1 = wb_prev_write & (wb_prev_robid == tag_rs1);
+    forward_prev_rs2 = wb_prev_write & (wb_prev_robid == tag_rs2);
+    forward_cur_rs1 = wb_write & (wb_robid[6:0] == tag_rs1);
+    forward_cur_rs2 = wb_write & (wb_robid[6:0] == tag_rs2);
+    rat_rs1_valid = forward_cur_rs1 | forward_prev_rs1 | valid_rs1;
+    rat_rs2_valid = forward_cur_rs2 | forward_prev_rs2 | valid_rs2;
+    casez({committed_rs1, valid_rs1})
+      2'b00: rat_rs1_tagval = (forward_prev_rs1 ? wb_prev_result : 
+                                (forward_cur_rs1 ? wb_result : tag_rs1));
+      2'b01: rat_rs1_tagval = spec_val_rs1;
+      2'b10: rat_rs1_tagval = comm_val_rs1;
+      default: rat_rs1_tagval = 32'bx;
+    endcase
+    casez({committed_rs2, valid_rs2})
+      2'b00: rat_rs2_tagval = (forward_prev_rs2 ? wb_prev_result :
+                                (forward_cur_rs2 ? wb_result : tag_rs2));
+      2'b01: rat_rs2_tagval = spec_val_rs2;
+      2'b10: rat_rs2_tagval = comm_val_rs2;
+      default: rat_rs1_tagval = 32'bx;
+    endcase
   end
   
 endmodule
