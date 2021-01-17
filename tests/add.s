259a4c749d68c047565e5216fb561e2fce4fcfc4
Sam Castleberry <castleberrysam@gmail.com>
2021-01-16 21:23:53 -0500

    Make add test more interesting

--- tests/add.s
+++ tests/add.s
@@ -1,11 +1,20 @@
 	.text
+	li	x1, 1
+	li	x2, 2
 	add	x0, x1, x2
+
+	li	x4, 3
+	li	x5, 4
 	add	x3, x4, x5
+
+	li	x7, 5
+	li	x8, 6
 	add	x6, x7, x8
+
+	li	x10, 7
+	li	x11, 8
 	add	x9, x10, x11
+
+	li	x13, 9
+	li	x14, 10
 	add	x12, x13, x14
-	add	x15, x16, x17
-	add	x18, x19, x20
-	add	x21, x22, x23
-	add	x24, x25, x26
-	add	x27, x28, x29
