4614433575779ac638ee11783376ccf97b5408a5
Sam Castleberry <castleberrysam@gmail.com>
2021-01-23 17:15:31 -0500

    Fix bug in right arithmetic shift

--- behavioral/scalu.v
+++ behavioral/scalu.v
@@ -63,7 +63,7 @@ module scalu(
       3'b010: scalu_result = ($signed(op1) < $signed(op2)); // SLT
       3'b011: scalu_result = (op1 < op2); // SLTU
       3'b100: scalu_result = (op[3] ? (op1 == op2) : (op1 ^ op2)); // XOR, SEQ
-      3'b101: scalu_result = (op[3] ? (op1 >> op2[4:0]) : ($signed(op1) >>> op2[4:0])); // SRL, SRA
+      3'b101: scalu_result = (op[3] ? $signed($signed(op1) >>> op2[4:0]) : (op1 >> op2[4:0])); // SRL, SRA
       3'b110: scalu_result = (op1 | op2);
       3'b111: scalu_result = (op1 & op2);
       default: scalu_result = 32'bx;
