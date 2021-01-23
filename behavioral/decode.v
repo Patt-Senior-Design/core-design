499e05eb3b6906ea8196405796159ec54c889912
Arjun Ramesh <arjunramesh@utexas.edu>
2021-01-23 15:54:00 +0530

    Fixed decode alternate rsop bug

--- behavioral/decode.v
+++ behavioral/decode.v
@@ -108,7 +108,7 @@ module decode(
 
   wire [2:0] brop;
   assign brop = {~|funct3[2:1],funct3[2:1]};
-
+  
   wire [4:0] rs1, rs2, rd;
   assign rs1 = insn[19:15];
   assign rs2 = insn[24:20];
@@ -251,8 +251,8 @@ module decode(
         rsop = 5'b10000;
       fmt_b:
         rsop = {2'b01,brop};
-      default:
-        rsop = {1'b0,fmt_r & insn[30],funct3};
+      default: // SRLI alternation: special case
+        rsop = {1'b0, ((fmt_r | (funct3 == 3'b101)) & insn[30]),funct3};
     endcase
 
   always @(posedge clk)
