df16eb4a0f6fe399e77b5156423cc7a33a046412
Arjun Ramesh <arjunramesh@utexas.edu>
2021-01-25 21:26:11 +0530

    Added minstret test

--- tests/csr.s
+++ tests/csr.s
@@ -7,16 +7,18 @@ main:
   li  x2, -743
 	mulh	x3, x1, x2
   ;
-  csrrw x4, mcycle, x0
+  csrrs x4, mcycle, x2
   ;
   li x5, 100
   li x5, 101
   ;
-  csrrs x4, minstret, x2
+  csrrc x4, minstret, x1
   ;
-  li x5, 100
-  li x5, 101
-
+  li x5, 200
+  li x5, 201
+  ;
+  csrrw x7, minstret,  x0
+  ;
   mv a0, zero
   mv ra, x31
   ret
