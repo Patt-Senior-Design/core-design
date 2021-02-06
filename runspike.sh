59bfb88bea26323de0d5ecbc524fdafdaaa17dec
Arjun Ramesh <arjunramesh@utexas.edu>
2021-02-06 22:11:04 +0530

    Added pfd extension to runspike

--- runspike.sh
+++ runspike.sh
@@ -11,4 +11,5 @@ exec spike --isa=RV32IM \
     -m0x10000000:0x10000,0x20000000:0x400000,0x30000000:0x1000 \
     --extlib="$DIR/plugins/uart.so" --device=uart,0x30010000 \
     --extension=lbcmp \
+    --extension=pfd \
     "$@"
