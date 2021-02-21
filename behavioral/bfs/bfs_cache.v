6b1c412781b2ccbf04a6ff94b290c24f8848e4c1
Arjun Ramesh <arjunramesh@utexas.edu>
2021-02-21 15:42:04 +0530

    Re-definition of test cache interface

--- behavioral/bfs/bfs_cache.v
+++ behavioral/bfs/bfs_cache.v
@@ -3,14 +3,13 @@ module bfs_cache (
   input rst,
 
   // Sync for request
-  input bfs_req,
-  input [31:0] bfs_req_addr,
-  output cache_ready,
-  output cache_buf_empty,
-
+  input bfs_dc_req,
+  input [31:0] bfs_dc_addr,
+  output dc_ready,
+  output dc_rbuf_empty,
   // Sync for result
-  output cache_fs,
-  output [63:0] cache_rdata);
+  output dc_fs,
+  output [63:0] dc_rdata);
 
   reg [63:0] storage [511:0]; // Store 8-Byte elements. 12-bit (4096) address space, byte-addressable
   integer i;
@@ -19,9 +18,9 @@ module bfs_cache (
     for (i = 0; i < 4096; i=i+1) begin
       //storage[i/8][(i & 3'b111)*8 +: 8]  = (i & 8'hFF);
       if (i % 8)
-        storage[i/8] += i;
+        storage[i/8] += (i << 32);
       else
-        storage[i/8] = 0;
+        storage[i/8] = (1 << 32);
     end
   end
 
@@ -39,26 +38,26 @@ module bfs_cache (
       req <= 0;
       counter <= 0;
     end else begin
-      req <= bfs_req;
+      req <= bfs_dc_req;
 
       if (~ready | counter[0]) begin
         counter <= counter - 1;
         rdata <= storage[(addr_base >> 6) + 8 - counter]; // Get 8 bytes
         if (counter[3]) // Mark on first iteration
-          storage[addr_base >> 6] <= storage[addr_base >> 6] | (1 << 32);
+          storage[addr_base >> 6] <= storage[addr_base >> 6] | (63'h01);
       end
 
-      if (bfs_req) begin
+      if (bfs_dc_req) begin
         counter <= 8;
-        addr_base <= bfs_req_addr;
+        addr_base <= bfs_dc_addr;
       end
     end
   end
 
-  assign cache_buf_empty = ~req & (~|counter);
-  assign cache_ready = ready;
-  assign cache_fs = &counter[2:0];
-  assign cache_rdata = rdata;
+  assign dc_rbuf_empty = ~req & (~|counter);
+  assign dc_ready = ready;
+  assign dc_fs = &counter[2:0];
+  assign dc_rdata = rdata;
 
 
 endmodule
