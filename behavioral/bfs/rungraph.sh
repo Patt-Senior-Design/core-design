bc775c0b4ea3155cea037262b8ed19e91c9e803b
Arjun Ramesh <arjunramesh@utexas.edu>
2021-02-22 19:44:00 +0530

    Added test script for bfs

---
+++ behavioral/bfs/rungraph.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+if [ $# -lt 2 ]; then
+  echo "Usage: rungraph.sh <from node (default: 3)> <to node (default: 2)>"
+fi
+
+DIR=$(dirname $0)
+
+# C arguments
+FROM_NODE=$1
+TO_NODE=$2
+
+# Verilog arguments
+FROM_NODE_VARG=""
+TO_NODE_VARG=""
+if [[ -n $FROM_NODE  &&  -n $TO_NODE ]]; then
+  FROM_NODE_VARG="+from=$FROM_NODE"
+  TO_NODE_VARG="+to=$TO_NODE"
+fi
+
+
+make -C $DIR || exit $?
+
+# C Graph run 
+./graph $FROM_NODE $TO_NODE | tee graph.out
+GRAPHPID=$!
+if [ $? -eq 124 ]; then
+  exit 1
+fi
+
+# Verilog run
+TIMEOUT=10
+timeout $TIMEOUT $DIR/top_bfs $FROM_NODE_VARG $TO_NODE_VARG
+SIMPID=$!
+if [ $? -eq 124 ]; then
+  exit 1
+fi
+
+
