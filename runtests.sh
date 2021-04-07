310dcaeb85f54db629213f7b27d21c38d6bc998a
Arjun Ramesh <arjunramesh@utexas.edu>
2021-04-06 23:33:58 -0500

    RTL option for runtests

--- runtests.sh
+++ runtests.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
 DIR=$(dirname $0)
+MODEL=${1:-behavioral}
+echo "Running $MODEL model"
 TESTS="$(basename -s .s $DIR/tests/*.s) $(basename -s .c $DIR/tests/*.c) $(basename -s .cpp $DIR/tests/*.cpp)"
 for TEST in $TESTS; do
     if [ $TEST = startup -o $TEST = stdlib ]; then continue; fi
 
     printf "%-16s" $TEST
-    $DIR/runtest.sh $TEST > /dev/null 2>&1
+    $DIR/runtest.sh $TEST $MODEL > /dev/null 2>&1
     if [ $? -eq 0 ]; then
         echo "passed"
     else
