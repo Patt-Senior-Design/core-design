ea4e50a71bd7f67270e29e69274ac853df795687
Arjun Ramesh <arjunramesh@utexas.edu>
2021-05-03 14:12:42 -0500

    perf tools

---
+++ perf/gen_hashasm.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+DIR=$(dirname $0)
+SEED_CT=$1
+
+#if [ $# -lt 1 ]; then
+#  echo "Usage: gen_hashasm.sh <number of seeded tests>"
+#  exit 1
+#fi
+
+make -C $DIR/exec clean > /dev/null || exit $?
+#for n in $(seq $SEED_CT); do
+riscv64-unknown-elf-gcc -march=rv32im -mabi=ilp32 -Wall -O2 -ffunction-sections \
+                                     -fdata-sections  -S -o $DIR/exec/hashset.s $DIR/hashset.c
+$DIR/replace_func.py $DIR/exec/fasthash.s $DIR/exec/hashset.s $DIR/findfast.S
+#done
+
+make -C $DIR/exec > /dev/null || exit $? 
+
+echo "Generated perf executables"
